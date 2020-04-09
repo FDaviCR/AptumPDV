df1e2328d1a1428a42091e3fa83501fc3f9e3dd0
Davi <davicamiloribeiro@gmail.com>
2020-04-09 19:59:03 -0300

    Publicar sistema

--- database/database.js
+++ database/database.js
@@ -1,7 +1,18 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('aptumpdv','root','',{
-    host: 'localhost',
+/*
+const connection = new Sequelize(
+    'aptumpdv','root','',
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        timezone: "-03:00"
+    }
+);
+*/
+
+const connection = new Sequelize('aptumpdv','davi','senhateste',{
+    host: 'mysql669.umbler.com',
     dialect: 'mysql',
     timezone: "-03:00"
 });
